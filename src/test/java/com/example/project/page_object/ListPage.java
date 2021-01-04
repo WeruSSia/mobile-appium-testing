6d3f7cd312caae4732e44dfb78b21b5155aa8c6e
WeruSSia <werussia16@gmail.com>
2021-01-04 12:33:23 +0100

    ItemPage created from ListPage

--- src/test/java/com/example/project/page_object/ListPage.java
+++ src/test/java/com/example/project/page_object/ListPage.java
@@ -7,8 +7,6 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.touch.LongPressOptions;
 import io.appium.java_client.touch.offset.ElementOption;
-import io.appium.java_client.touch.offset.PointOption;
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 import java.time.Duration;
@@ -24,22 +22,10 @@ public class ListPage {
 
     @AndroidFindBy(id = "com.slava.buylist:id/editText1")
     private AndroidElement addItemEditText;
-    @AndroidFindBy(id = "com.slava.buylist:id/editText2")
-    private AndroidElement priceEditText;
-    @AndroidFindBy(id = "com.slava.buylist:id/editText3")
-    private AndroidElement amountEditText;
-    @AndroidFindBy(id = "com.slava.buylist:id/spinner1")
-    private AndroidElement unitSpinner;
-    @AndroidFindBy(xpath = "//android.widget.FrameLayout")
-    private AndroidElement unitChoiceFrameLayout;
-    @AndroidFindBy(id = "com.slava.buylist:id/spinner2")
-    private AndroidElement categorySpinner;
-    @AndroidFindBy(id = "com.slava.buylist:id/button2")
-    private AndroidElement addItemButton;
-    @AndroidFindBy(xpath = "//android.widget.FrameLayout")
-    private AndroidElement categoryChoiceFrameLayout;
-    @AndroidFindBy(xpath = "//android.widget.RelativeLayout[1]//android.widget.RelativeLayout")
+    @AndroidFindBy(xpath = "//android.widget.ListView/android.widget.RelativeLayout")
     private AndroidElement firstItem;
+    @AndroidFindBy(xpath = "//android.widget.ListView/android.widget.LinearLayout[3]")
+    private AndroidElement editItemLinearLayout;
 
 
     public ListPage enterItemName(String itemName) {
@@ -47,41 +33,14 @@ public class ListPage {
         return this;
     }
 
-    public ListPage enterPrice(String price) {
-        priceEditText.sendKeys(price);
-        return this;
-    }
-
-    public ListPage enterAmount(String amount) {
-        amountEditText.sendKeys(amount);
-        return this;
-    }
-
-    public ListPage chooseUnit(String index) {
-        unitSpinner.click();
-        unitChoiceFrameLayout.findElementByXPath("//android.widget.CheckedTextView[" + index + "]").click();
-        return this;
-    }
-
-    public ListPage chooseCategory(String index) {
-        categorySpinner.click();
-
-        categoryChoiceFrameLayout.findElementByXPath("//android.widget.CheckedTextView[" + index + "]").click();
-        return this;
-    }
-
-    public ListPage clickAddItemButton() {
-        addItemButton.click();
-        return this;
-    }
-
     public ListPage editFirstItem() {
         new TouchAction(driver)
                 .longPress(new LongPressOptions()
                         .withElement(ElementOption.element(firstItem))
-                        .withDuration(Duration.ofMillis(3000)))
+                        .withDuration(Duration.ofMillis(2000)))
                 .release()
                 .perform();
+        editItemLinearLayout.click();
         return this;
     }
 
