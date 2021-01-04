9f83e995227eaac5f95d784c5b600b53469cb84e
WeruSSia <werussia16@gmail.com>
2021-01-04 13:41:01 +0100

    ListPage locators simplified

--- src/test/java/com/example/project/page_object/ListPage.java
+++ src/test/java/com/example/project/page_object/ListPage.java
@@ -20,12 +20,62 @@ public class ListPage {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
-    @AndroidFindBy(id = "com.slava.buylist:id/editText1")
+    @AndroidFindBy(id = "editText1")
     private AndroidElement addItemEditText;
     @AndroidFindBy(xpath = "//android.widget.ListView/android.widget.RelativeLayout")
     private AndroidElement firstItem;
     @AndroidFindBy(xpath = "//android.widget.ListView/android.widget.LinearLayout[3]")
     private AndroidElement editItemLinearLayout;
+    @AndroidFindBy(id = "editText2")
+    private AndroidElement priceEditText;
+    @AndroidFindBy(id = "editText3")
+    private AndroidElement amountEditText;
+    @AndroidFindBy(id = "spinner1")
+    private AndroidElement unitSpinner;
+    @AndroidFindBy(xpath = "//android.widget.FrameLayout")
+    private AndroidElement unitChoiceFrameLayout;
+    @AndroidFindBy(id = "spinner2")
+    private AndroidElement categorySpinner;
+    @AndroidFindBy(id = "button2")
+    private AndroidElement addItemButton;
+    @AndroidFindBy(xpath = "//android.widget.FrameLayout")
+    private AndroidElement categoryChoiceFrameLayout;
+    @AndroidFindBy(id = "editText4")
+    private AndroidElement commentEditText;
+    @AndroidFindBy(id = "button2")
+    private AndroidElement saveItemButton;
+    @AndroidFindBy(xpath = "//android.widget.ListView/android.widget.LinearLayout[4]")
+    private AndroidElement removeItemLinearLayout;
+    @AndroidFindBy(id = "button1")
+    private AndroidElement confirmRemovalButton;
+
+    public ListPage enterPrice(String price) {
+        priceEditText.sendKeys(price);
+        return this;
+    }
+
+    public ListPage enterAmount(String amount) {
+        amountEditText.sendKeys(amount);
+        return this;
+    }
+
+    public ListPage chooseUnit(String index) {
+        unitSpinner.click();
+        unitChoiceFrameLayout.findElementByXPath("//android.widget.CheckedTextView[" + index + "]").click();
+        return this;
+    }
+
+    public ListPage chooseCategory(String index) {
+        categorySpinner.click();
+
+        categoryChoiceFrameLayout.findElementByXPath("//android.widget.CheckedTextView[" + index + "]").click();
+        return this;
+    }
+
+    public ListPage clickAddItemButton() {
+        addItemButton.click();
+        return this;
+    }
 
 
     public ListPage enterItemName(String itemName) {
@@ -33,14 +83,38 @@ public class ListPage {
         return this;
     }
 
-    public ListPage editFirstItem() {
+    public ListPage longPressOnFirstItem() {
         new TouchAction(driver)
                 .longPress(new LongPressOptions()
                         .withElement(ElementOption.element(firstItem))
                         .withDuration(Duration.ofMillis(2000)))
                 .release()
                 .perform();
+        return this;
+    }
+
+    public ListPage clickOnEdit() {
         editItemLinearLayout.click();
+        return this;
+    }
+
+    public ListPage enterComment(String comment) {
+        commentEditText.sendKeys(comment);
+        return this;
+    }
+
+    public ListPage saveItem() {
+        saveItemButton.click();
+        return this;
+    }
+
+    public ListPage clickOnRemove() {
+        removeItemLinearLayout.click();
+        return this;
+    }
+
+    public ListPage confirmRemoval() {
+        confirmRemovalButton.click();
         return this;
     }
 
